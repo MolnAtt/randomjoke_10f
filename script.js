ebaca6529ba4950f29f5cf01b324aa7e371901dc
Molnár Attila <molnar.attila@szlgbp.hu>
2024-01-23 14:07:47 +0100

    gáz viccek

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
-kerdojel.addEventListener('click',
-    e => {
-        alert("halihó");
-        console.log(e.target);
-        alert("lenyomták a gombot, aminek a felirata ez: " + e.target.innerHTML);
+
+
+ujviccgomb.addEventListener('click',
+    async e => {
+        if(!valaszdiv.classList.contains('nemlatszik'))
+        {
+            valaszdiv.classList.toggle('nemlatszik');
+        }
+
+        let szervervalasz_szotar = await olvaso_fetch('https://official-joke-api.appspot.com/random_joke');
+
+        kerdesdiv.innerHTML = szervervalasz_szotar['setup'];
+        valaszdiv.innerHTML = szervervalasz_szotar['punchline'];
     }
 );
+
+
+
+kerdojel.addEventListener('click',
+    e => {
+        valaszdiv.classList.toggle('nemlatszik');
+    }
+);
+
+
+async function olvaso_fetch(url){
+    const response = await fetch(url);
+    const json_promise = await response.json();
+    return json_promise;
+}
